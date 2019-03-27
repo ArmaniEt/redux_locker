c64a68141a401da23b17b880909a7576437bfbe7
armani <razzarioa@gmail.com>
2019-03-27 13:18:37 +0600

    created reducer to change state and also actions, installed redux and react-redux also created Locker_api to displayinterface, wrapped App to Provider HOC passed store there

---
+++ src/containers/Locker/Locker_api.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+
+class Locker extends Component {
+    render() {
+        return <div>
+
+        </div>
+    }
+}
+
+
+export default connect()(Locker);
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    digits: ""
+};
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
